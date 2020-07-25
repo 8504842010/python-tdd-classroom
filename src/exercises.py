06997a7d67bc01a4bde4255151c814325ec59ffe
feature/implement-functions <98yashgoyal.com>
2020-07-25 15:51:45 +0530

    required changes have been done again

--- src/exercises.py
+++ src/exercises.py
@@ -1,17 +1,11 @@
 def reverse_list(input_list):
     """
-    Reverses  the order of elements in list input_list.
+    Reverses order of elements in list input_list.
     """
-    length = len(input_list) - 1
-    l1 = []
-    while length >= 0:
-        l1.append(input_list[length])
-        length = length - 1
-    return l1
-
+    return None
 
 def count_digits(number):
-    str1=str(number)
-    lgth=len(str1)
-    return lgth
-
+    """
+    Return count of digits
+    """
+    return None
--- src/string_exercise.py
+++ src/string_exercise.py
@@ -4,39 +4,28 @@ class StringExercise:
         pass   # Do some initial setup in this constructor method, if needed
 
     def reverse_string(self, input_str):
-        str1=input_str[::-1]
-        return str1
+        """
+        Reverses order of characters in string input_str.
+        """
+        return None
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        if character=="A" or character=="I" or character=="O" or character=="E" or character=="U":
-            return True
-        elif character=="a" or character=="i" or character=="o" or character=="e" or character=="u":
-            return True
-        else:
-            return False
-
+        return None
 
     def find_longest_word(self, sentence):
-
-        current_max = 0
-        for v in sentence.split():
-            le = len(v)
-            if le > current_max:
-                current_max = le
-                word = v
-        return word
+        """
+        Returns the longest word in string sentence.
+        In case there are several, return the first.
+        """
+        return None
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        list1=[]
-        for v in text.split():
-            list1.append(len(v))
-
-        return list1
+        return None
